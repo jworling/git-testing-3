8c1f8e8c9645b058459ed4f895fba6648c538ed4
Jamie Worling (BOUW) <jamie.worling@aweburn.com>
2017-07-28 10:23:28 -0400

    Initial Commit

---
+++ GitTesting3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTesting3
+//
+//  Created by Jamie on 28-07-17.
+//  Copyright © 2017 awburn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
