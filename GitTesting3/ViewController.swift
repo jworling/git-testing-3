d4502117c14e60c98e517b1f2df8d45295f32b23
Jamie Worling (BOUW) <jamie.worling@aweburn.com>
2017-07-28 10:26:00 -0400

    add print - "line 1"

--- GitTesting3/ViewController.swift
+++ GitTesting3/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print ("line 1")
+        
     }
 
     override func didReceiveMemoryWarning() {
